f0128d0c7cc2b1ca9410467e86bbf78ea1147d08
RalphUnity <55875029+RalphUnity@users.noreply.github.com>
2019-09-30 14:59:28 +0800

    Added Player Movement and Camera Follow

---
+++ Circle of Death/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    public float movementSpeed;
+    public float hitDist = 0.0f;
+
+    public GameObject bulletSpawnPoint;
+    //Methods
+    void Update()
+    {
+        //Player facing mouse
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (playerPlane.Raycast(ray, out hitDist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+        }
+
+
+        //Player Movement
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+    }
+}
