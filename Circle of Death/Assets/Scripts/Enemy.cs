c2cf2df145e0a03be2217415e1bb55ce650702aa
RalphUnity <55875029+RalphUnity@users.noreply.github.com>
2019-09-30 16:31:37 +0800

    Added Enemy and Scoring System

---
+++ Circle of Death/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    //Variables
+    public float health;
+    public float pointsToGive;
+
+    public GameObject player;
+
+
+    //Methods
+    public void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+
+        player.GetComponent<Player>().points += pointsToGive;
+    }
+}
--- Circle of Death/Assets/Scripts/Player.cs
+++ Circle of Death/Assets/Scripts/Player.cs
@@ -6,9 +6,15 @@ public class Player : MonoBehaviour
 {
     //Variables
     public float movementSpeed;
-    public float hitDist = 0.0f;
+    public float waitTime;
+    public float points;
 
     public GameObject bulletSpawnPoint;
+    public GameObject playerObj;
+    public GameObject bullet;
+
+    private float hitDist = 0.0f;
+
     //Methods
     void Update()
     {
@@ -16,13 +22,13 @@ public class Player : MonoBehaviour
         Plane playerPlane = new Plane(Vector3.up, transform.position);
         Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (playerPlane.Raycast(ray, out hitDist))
+        if(playerPlane.Raycast(ray, out hitDist))
         {
             Vector3 targetPoint = ray.GetPoint(hitDist);
             Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
             targetRotation.x = 0;
             targetRotation.z = 0;
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 7f * Time.deltaTime);
         }
 
 
@@ -30,5 +36,24 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
             transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
 
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+
+        //Shooting
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet.transform, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
     }
 }
